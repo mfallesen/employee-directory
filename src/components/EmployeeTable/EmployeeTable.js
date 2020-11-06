0f2c8a8037b063ea97a180bea7484190dbbc66f6
Mike Fallesen <mfallesen2010@gmail.com>
2020-11-06 09:31:50 -0800

    refactor employee table component

--- src/components/EmployeeTable/EmployeeTable.js
+++ src/components/EmployeeTable/EmployeeTable.js
@@ -1,34 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react'
 import EmployeeCards from '../EmployeeCards/EmployeeCards';
 import "./EmployeeTable.css"
 
-export default function EmployeeTable(props) {
-    return (
-        <div>
-            <table className="table">
-                <thead className="thead-light">
-                    <tr>
-                        <th scope="col">Picture</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Email</th>
-                        <th scope="col">Phone</th>
-                        <th scope="col">DOB</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <EmployeeCards
-            picture={"pic"}
-            first={"first"}
-            last={"last"}
-            email={"email"}
-            phone={"phone"}
-            dob={"dob"}
-            />
 
-                </tbody>
-               
-            </table>
-        </div>
-    )
+
+
+export default class EmployeeTable extends Component {
+    render() {
+        return (
+            <div>
+                <table className="table">
+                    <thead className="thead-light">
+                        <tr>
+                            <th scope="col">Picture</th>
+                            <th scope="col">First</th>
+                            <th scope="col">Last</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Phone</th>
+                            <th scope="col">DOB</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <EmployeeCards
+                            picture={"pic"}
+                            first={"first"}
+                            last={"last"}
+                            email={"email"}
+                            phone={"phone"}
+                            dob={"dob"}
+                        />
+
+                    </tbody>
+
+                </table>
+            </div>
+        )
+    }
 }
